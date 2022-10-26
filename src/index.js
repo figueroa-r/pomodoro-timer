5e92e77cec8920d0df02815c02dfe0ea86dde1b3
Rafael Figueroa <r.figueroa93.rf@gmail.com>
2022-10-26 15:22:15 -0700

    removed comment

--- src/index.js
+++ src/index.js
@@ -113,4 +113,4 @@ class Pomodoro extends React.Component {
 /* ======================================== */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(/*insert my Parent component here */ <Pomodoro />);
+root.render(<Pomodoro />);
